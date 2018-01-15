754bfffc05925c1cbcdf0a54959932e22ccf96df
Squishy47 <Squishy47@users.noreply.github.com>
2018-01-15 20:20:12 +0000

    fixed bug that caused buffer to skip index 0.

--- CircularBuffer.h
+++ CircularBuffer.h
@@ -19,30 +19,30 @@ class CircularBuffer{
     
 public:
     void init(int inValue){
-        buffer.resize(inValue);
         bufferLength = inValue;
-        for(int i = 0; i < bufferLength - 1; i++)
+        buffer.resize(bufferLength);
+        for(int i = 0; i < bufferLength; i++)
             buffer[i] = 0.0;
     }
-    
+
     // num of elements to read behind current sample (n-j) input being j
     float read(int numElementsToRead){
         tail = head - numElementsToRead;    // tail = current index in buffer - number of elements to read.
         if (tail > bufferLength - 1)        // if tail == bufferLength minus buffer length = 0;
-            tail -= bufferLength - 1;
+            tail -= bufferLength;
         else if (tail < 0)
-            tail += bufferLength - 1;
+            tail += bufferLength;
         return buffer[tail];
     }
     
     // writes the new samples in the next index of the array,
     // if the index is greater than array, it loops to the index 0.
-    void write(float inL, int numElementsToWrite){
+    void write(float inValue, int numElementsToWrite){
         for(int i = 0; i < numElementsToWrite; i++){
             if(head > bufferLength - 1)
-                head -= bufferLength - 1;
-            if(head < 0) head = 0;
-                buffer[head] = inL;
+                head -= bufferLength;
+            else if(head < 0) tail += bufferLength;
+                buffer[head] = inValue;
             head++;
         }
     }
